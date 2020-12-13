ab86abe6ccf42f87a03a0fd608c480a8cb5f059e
pialnoman <awronno.adhar@gmail.com>
2020-12-13 12:12:27 +0600

    product list page added

---
+++ src/app/pages/product-list-page/product-list-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListPageComponent } from './product-list-page.component';
+
+
+
+@NgModule({
+  declarations: [ProductListPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductListPageModule { }
