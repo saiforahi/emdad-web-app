569dd5feee82f6a342deab5a9e15e48651ebc307
pial_noman <awronno.adhar@gmail.com>
2021-01-17 21:45:02 +0600

    2nd push

--- src/app/pages/product-list-page/product-list-page.component.ts
+++ src/app/pages/product-list-page/product-list-page.component.ts
@@ -14,12 +14,13 @@ export class ProductListPageComponent implements OnInit {
   products = [];
   prodEnd;
   nextBatchProdLink;
-  isCategory = false;
   categoryId;
   prevParam;
   prodInRow6;
   panelOpenState = true;
   categories: any;
+  expandedCat;
+  expandedSubCat;
 
   constructor(
     private getProduct: GetProductService,
@@ -28,76 +29,70 @@ export class ProductListPageComponent implements OnInit {
     private getCategories: GetCategoryService
   ) {
     this.route.paramMap.subscribe((params) => {
-      console.log(params);
       this.ngOnInit();
     });
   }
 
   ngOnInit(): void {
-    if(this.router.url.split('/').length > 2){
+    window.scrollTo(0, 0);
+    if (this.router.url.split('/').length > 2) {
       this.prodInRow6 = false;
     }
-    this.getCategories.category().subscribe(item =>{
+    this.getCategories.category().subscribe((item) => {
       console.log(item);
       this.categories = item;
-    })
-    console.log(this.prodInRow6);
+    });
     this.sellerId = this.route.snapshot.params['id'];
     this.categoryId = this.route.snapshot.params['id'];
+    this.expandedCat = parseInt(localStorage.getItem("expandedCat"));
+    this.expandedSubCat = parseInt(localStorage.getItem("expandedSubCat"));
     // check for is seller ==>
     const getUrlStr = this.router.url;
     const newstr = getUrlStr.substr(10, 6);
     this.isSeller = newstr == 'seller' ? true : false;
     // <==
     // if its not the seller-page then its the category page
-    if (this.isSeller == false) this.isCategory = true;
-
-    // get product by seller
-    this.getProduct.getProductBySeller(this.sellerId).subscribe((item) => {
-      this.products = item.data.results;
-      this.nextBatchProdLink = item.data.links.next;
-      // console.log(this.products);
-    });
-
-    // get product by category
-    this.getProduct.getProductByCategory(this.categoryId).subscribe((item) => {
-      console.log(item);
-      this.products = item.data.results;
-      this.nextBatchProdLink = item.data.links.next;
-      console.log(this.products);
-    });
-  }
-
-  getProdOnFilter(){
-    // get product by category
-    this.getProduct.getProductByCategory(this.categoryId).subscribe((item) => {
-      console.log(item);
-      this.products = item.data.results;
-      this.nextBatchProdLink = item.data.links.next;
-      console.log(this.products);
-    });
-  }
-
-  @HostListener('window:scroll')
-  onWindowScroll() {
-    //In chrome and some browser scroll is given to body tag
-    let pos =
-      (document.documentElement.scrollTop || document.body.scrollTop) +
-      document.documentElement.offsetHeight;
-    let max = document.documentElement.scrollHeight;
-    // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
-    if (pos >= max - 1 && this.nextBatchProdLink != null) {
-      //Do your action here
-      console.log('reached bootm');
-      this.getProduct
-        .getNextBatchProduct(this.nextBatchProdLink)
-        .subscribe((item) => {
-          this.products = [...this.products, ...item.data.results];
-          this.nextBatchProdLink = item.data.links.next;
-        });
-    }
-    if (this.nextBatchProdLink == null) {
-      this.prodEnd = true;
+    if (this.isSeller == true) {
+      // get product by seller
+      this.getProduct.getProductBySeller(this.sellerId).subscribe((item) => {
+        this.products = item.data.results;
+        this.nextBatchProdLink = item.data.links.next;
+      });
+    }else {
+      // get product by category
+      this.getProduct.getProductByCategory(this.categoryId).subscribe((item) => {
+        this.products = item.data.results;
+        this.nextBatchProdLink = item.data.links.next;
+      });
     }
   }
+
+  getProdOnFilter(ChildCatId, subCatId, catId) {
+    this.router.navigate(['/products/category/', ChildCatId]);
+    localStorage.setItem("expandedSubCat", subCatId);
+    localStorage.setItem("expandedCat", catId);
+  }
+
+  // @HostListener('window:scroll')
+  // onWindowScroll() {
+  //   //In chrome and some browser scroll is given to body tag
+  //   let pos =
+  //     (document.documentElement.scrollTop || document.body.scrollTop) +
+  //     document.documentElement.offsetHeight;
+  //   let max = document.documentElement.scrollHeight;
+  //   // pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+  //   if (pos >= max - 1 && this.nextBatchProdLink != null) {
+  //     //Do your action here
+  //     console.log('reached bootm');
+  //     this.getProduct
+  //       .getNextBatchProduct(this.nextBatchProdLink)
+  //       .subscribe((item) => {
+  //         this.products = [...this.products, ...item.data.results];
+  //         this.nextBatchProdLink = item.data.links.next;
+  //       });
+  //   }
+  //   if (this.nextBatchProdLink == null) {
+  //     this.prodEnd = true;
+  //   }
+  // }
 }
