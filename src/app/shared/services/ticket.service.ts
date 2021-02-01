37a3cc56ad700a67b68827cf22f98d516f168409
hpdipto <haris.dipto@gmail.com>
2021-02-01 20:59:20 +0600

    raise a support ticket page completed

--- src/app/shared/services/ticket.service.ts
+++ src/app/shared/services/ticket.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class TicketService {
-  private readonly URL = 'http://127.0.0.1:8000/api/issue/add/';
+  private readonly URL_issue_add = 'http://127.0.0.1:8000/api/issue/add/';
+
   constructor(private http: HttpClient) {}
 
   openTicket(ticketForm: any): Observable<any> {
@@ -15,6 +16,17 @@ export class TicketService {
         Authorization: 'Bearer ' + localStorage.getItem('token'),
       }),
     };
-    return this.http.post(this.URL, ticketForm, httpOptions);
+    return this.http.post(this.URL_issue_add, ticketForm, httpOptions);
+  }
+
+  getTickets(uid): Observable<any> {
+    // let uid = localStorage.getItem('uid');
+    let URL_issue_list = `http://127.0.0.1:8000/api/issue/list/${uid}/`;
+    let httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      }),
+    };
+    return this.http.get(URL_issue_list, httpOptions);
   }
 }
