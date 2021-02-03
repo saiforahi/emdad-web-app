0c9d667d789b5f94c712d59bc9ba8910322f5f81
pial_noman <awronno.adhar@gmail.com>
2021-02-03 13:07:18 +0600

    quotations service merged

--- src/app/shared/services/quotation.service.ts
+++ src/app/shared/services/quotation.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import {config} from '../../../config';
+import { config } from '../../../config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuotationService {
   httpOptions = {
@@ -14,7 +14,26 @@ export class QuotationService {
   };
   statusUpdated: BehaviorSubject<any> = new BehaviorSubject<any>(false);
   constructor(private http: HttpClient) {}
-  get_user_quotation_list():Observable<any>{
-      return this.http.get(config.base_url+"api/quote/user/wise/"+localStorage.getItem('uid')+"/",this.httpOptions)
+  get_user_quotation_list(): Observable<any> {
+    return this.http.get(
+      config.base_url +
+        'api/quote/user/wise/' +
+        localStorage.getItem('uid') +
+        '/',
+      this.httpOptions
+    );
+  }
+
+  createQuotation(rfqData: any): Observable<any> {
+    let httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      }),
+    };
+    return this.http.post(
+      config.base_url + '/api/quote/create/',
+      rfqData,
+      httpOptions
+    );
   }
 }
