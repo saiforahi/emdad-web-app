96f3289736ab6ab86ddddcc9c573a550cfb97854
saiforahi <shaiforahi@northsouth.edu>
2021-02-03 16:22:29 +0600

    Order history table update, buyer single order view, checkout page update

--- src/app/pages/buyer-order-history-details/buyer-order-history-details.component.ts
+++ src/app/pages/buyer-order-history-details/buyer-order-history-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { OrderService } from 'src/app/shared/services/order.service';
+import { config } from 'src/config';
 @Component({
   selector: 'app-buyer-order-history-details',
   templateUrl: './buyer-order-history-details.component.html',
@@ -8,15 +9,50 @@ import { OrderService } from 'src/app/shared/services/order.service';
 })
 export class BuyerOrderHistoryDetailsComponent implements OnInit {
   order_id:any;
-  order_details:any;
+  orders:any;
+  base_url:string;
+  subTotal:any;
+  discount:any;
+  vat:any;
+  total:any;
   constructor(private route:ActivatedRoute,private orderService:OrderService) { }
 
   ngOnInit(): void {
-    this.order_id=this.route.snapshot.params['id'];
+    this.base_url=config.base_url;
+    this.order_id=this.route.snapshot.params['order_id'];
     this.orderService.get_buyer_order_details(this.order_id).subscribe(
-      (success)=>console.log(success),
+      (success)=>{console.log(success.data);this.orders=success.data},
       (error)=>console.log(error)
     )
   }
 
+  calcTotalPrice() {
+    let subTotal = 0;
+    this.orders.forEach((element) => {
+        subTotal += parseFloat(element.unit_price) * parseFloat(element.quantity);
+    });
+    this.subTotal=subTotal;
+    return subTotal;
+  }
+
+  calcDiscount(){
+    let total=0;
+    this.orders.forEach(element => {
+      total+= parseInt(element.order.discount_coupon_amount)
+    });
+    this.discount=total;
+    return total;
+  }
+
+  calVat(){
+    let total=0;
+    this.orders.forEach(element => {
+      total+=parseInt(element.vat_amount)
+    });
+    this.vat=total;
+    return total;
+  }
+  calTotal(){
+    return this.vat+this.discount+this.subTotal;
+  }
 }
