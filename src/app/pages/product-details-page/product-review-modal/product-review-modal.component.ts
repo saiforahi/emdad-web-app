57d188bd51b2d3358e095486cd492de5d3914f9f
pial_noman <awronno.adhar@gmail.com>
2021-02-04 14:46:39 +0600

    EW-43

--- src/app/pages/product-details-page/product-review-modal/product-review-modal.component.ts
+++ src/app/pages/product-details-page/product-review-modal/product-review-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CustomerReviewService } from 'src/app/shared/services/customer-review.service';
+import { UserAuthService } from 'src/app/shared/services/user-auth.service';
 
 @Component({
   selector: 'app-product-review-modal',
@@ -6,14 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-review-modal.component.css']
 })
 export class ProductReviewModalComponent implements OnInit {
+  @Input() prodcutDetails;
+  userId: any;
 
-  constructor() { }
+  constructor(
+    private comments: CustomerReviewService,
+    private user: UserAuthService) { }
 
   ngOnInit(): void {
+    this.user.uId.subscribe(item => {
+      this.userId = item;
+    })
   }
 
   hide_review_modal(){
     document.getElementById('prodReviewModal').style.display="none";
+  }
+
+  addComments(review){
+    // const data = {"product": this.prodcutDetails.id, "buyer": this.userId, "comment": review}
+    this.comments.addComments({"product": this.prodcutDetails.id, "buyer": this.userId, "comment": review}).subscribe(item => {
+      console.log(item);
+    })
   }
 
 }
