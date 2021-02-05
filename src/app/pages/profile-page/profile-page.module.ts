79ee152f3fa6aab78b1964c4be62971e75b00089
hpdipto <haris.dipto@gmail.com>
2021-02-05 11:02:45 +0600

    quotation table & modal completed

--- src/app/pages/profile-page/profile-page.module.ts
+++ src/app/pages/profile-page/profile-page.module.ts
@@ -5,25 +5,25 @@ import { ProfilePageComponent } from './profile-page.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ChangePasswordFormComponent } from '../../components/change-password-form/change-password-form.component';
 import { EditProfileFormComponent } from '../../components/edit-profile-form/edit-profile-form.component';
-import {OrderHistoryComponent} from '../../components/order-history-table/order-history-table.component';
-import {ManageQuotations} from '../../components/manage-quotations-table/manage-quotations-table.component';
-import {ProfileLogoutModal} from '../../components/profile-logout-modal/profile-logout-modal.component';
-import {QuotationDetailsModal} from '../../components/quotation-details-modal/quotation-details-modal.component';
+import { OrderHistoryComponent } from '../../components/order-history-table/order-history-table.component';
+import { ManageQuotations } from '../../components/manage-quotations-table/manage-quotations-table.component';
+import { ProfileLogoutModal } from '../../components/profile-logout-modal/profile-logout-modal.component';
+// import {QuotationDetailsModal} from '../../components/quotation-details-modal/quotation-details-modal.component';
 
 const routes: Routes = [
-	{ path: '', component: ProfilePageComponent, pathMatch: 'full' },
+  { path: '', component: ProfilePageComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
-	declarations: [
-		ProfilePageComponent,
-		ChangePasswordFormComponent,
-		EditProfileFormComponent,
-		OrderHistoryComponent,
-		ManageQuotations,
-		ProfileLogoutModal,
-		QuotationDetailsModal
-	],
-	imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
+  declarations: [
+    ProfilePageComponent,
+    ChangePasswordFormComponent,
+    EditProfileFormComponent,
+    OrderHistoryComponent,
+    ManageQuotations,
+    ProfileLogoutModal,
+    // QuotationDetailsModal
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule],
 })
 export class ProfilePageModule {}
