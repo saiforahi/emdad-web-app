014d6a1d0d7bc65d02f6ebadcaeba2423506feb7
pial_noman <awronno.adhar@gmail.com>
2021-03-07 11:48:09 +0600

    update in side nav rout

---
+++ src/app/seller/pages/support-page/support-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SupportPageComponent } from './support-page.component';
+
+
+
+@NgModule({
+  declarations: [SupportPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SupportPageModule { }
