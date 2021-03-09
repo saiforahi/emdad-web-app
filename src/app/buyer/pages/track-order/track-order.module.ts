147ad19572bcf81268037e6efb350a36863fb102
saiforahi <shaiforahi@northsouth.edu>
2021-03-09 15:40:53 +0600

    product filtering in search page, login modal design changed, footer text

--- src/app/buyer/pages/track-order/track-order.module.ts
+++ src/app/buyer/pages/track-order/track-order.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TrackOrderRoutingModule } from './track-order-routing.module';
 import { TrackOrderComponent } from './track-order.component';
-
+import {MaterialModule} from '../../../material.module'
 
 @NgModule({
   declarations: [TrackOrderComponent],
   imports: [
     CommonModule,
-    TrackOrderRoutingModule
+    TrackOrderRoutingModule,
+    MaterialModule
   ]
 })
 export class TrackOrderModule { }
