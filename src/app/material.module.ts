f8429c36dc43dc9e63b545f051ade848ffb6f49e
saiforahi <shaiforahi@northsouth.edu>
2021-03-15 17:10:02 +0600

    address radio button selected color change

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -46,6 +46,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatStepperModule,
     MatIconModule,
     MatProgressBarModule,
+    MatChipsModule
   ],
   exports: [
     MatMenuModule,
