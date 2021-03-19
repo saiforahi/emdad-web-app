bc71a782c1096c27348aff4330749c29c9e487a6
pial_noman <awronno.adhar@gmail.com>
2021-03-19 23:59:58 +0600

    EW-124

--- src/app/buyer/pages/wishlist-page/wishlist-page.component.ts
+++ src/app/buyer/pages/wishlist-page/wishlist-page.component.ts
@@ -6,26 +6,53 @@ import { GetProductService } from '../../../shared/services/get-product.service'
 @Component({
   selector: 'app-wishlist-page',
   templateUrl: './wishlist-page.component.html',
-  styleUrls: ['./wishlist-page.component.css']
+  styleUrls: ['./wishlist-page.component.css'],
 })
 export class WishlistPageComponent implements OnInit {
   products = [];
-  
-  constructor(private wishlist: WishlistService, private product: GetProductService) { }
+  style: string;
+  nextBatchProdLink: any;
+  prodEnd: boolean;
+
+  constructor(
+    private wishlist: WishlistService,
+    private product: GetProductService
+  ) {}
 
   ngOnInit(): void {
-    this.wishlist.getWishlist().subscribe(item => {
-      console.log(item)
+    this.style = 'grid';
+    this.wishlist.getWishlist().subscribe((item) => {
+      console.log(item);
       var initProducts = item.data[0];
-      initProducts.forEach(element => {
-        console.log(element.product)
-        this.product.productDetails(element.product.id).subscribe(item2 => {
+      initProducts.forEach((element) => {
+        console.log(element.product);
+        this.product.productDetails(element.product.id).subscribe((item2) => {
           this.products.push(item2.data[0]);
-          console.log(item2.data[0])
-        })
+          console.log(item2.data[0]);
+        });
       });
-    })
-    console.log(this.products)
+    });
+    console.log(this.products);
   }
 
+  getNextBatchproduct() {
+    if (this.nextBatchProdLink != null) {
+      //Do your action here
+      console.log('reached bootm');
+      this.product
+        .getNextBatchProduct(this.nextBatchProdLink)
+        .subscribe((item) => {
+          this.products = [...this.products, ...item.data.results];
+          this.nextBatchProdLink = item.data.links.next;
+        });
+    }
+    if (this.nextBatchProdLink == null) {
+      this.prodEnd = true;
+      // this.openSnackBar('No more product to show!', 'OK');
+    }
+  }
+
+  set_style(value: string) {
+    this.style = value;
+  }
 }
