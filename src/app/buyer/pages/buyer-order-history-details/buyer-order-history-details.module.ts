b7db5a7f3520d1585f7bd668ee8b98580ce2f91a
Shaif Azad <shaif.rahi@northsouth.edu>
2021-03-20 19:03:50 +0600

    order invoice pdf issue

--- src/app/buyer/pages/buyer-order-history-details/buyer-order-history-details.module.ts
+++ src/app/buyer/pages/buyer-order-history-details/buyer-order-history-details.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgxSpinnerModule } from "ngx-spinner";
 import { BuyerOrderHistoryDetailsRoutingModule } from './buyer-order-history-details-routing.module';
 import { BuyerOrderHistoryDetailsComponent } from './buyer-order-history-details.component';
 import { AngularCreatePdfModule } from 'angular-create-pdf';
 @NgModule({
   declarations: [BuyerOrderHistoryDetailsComponent],
   imports: [
-    CommonModule,AngularCreatePdfModule,
+    CommonModule,AngularCreatePdfModule,NgxSpinnerModule,
     BuyerOrderHistoryDetailsRoutingModule
   ]
 })
