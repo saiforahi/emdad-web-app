b7b28b8aeb3f7cf1dd1f31a6ae7fb609ccb1bb7d
Pranto <prakat1620@gmail.com>
2021-03-20 23:01:42 +0600

    manage rfq page buyer side responsive and ui fixed

--- src/app/buyer/pages/rfq-page/rfq-page.module.ts
+++ src/app/buyer/pages/rfq-page/rfq-page.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
-
+import{ CommonuiModule} from '../../../ui/commonui/commonui.module';
 import { RfqPageComponent } from './rfq-page.component';
 
 const routes: Routes = [
@@ -17,6 +17,7 @@ const routes: Routes = [
     FormsModule,
     RouterModule.forChild(routes),
     NgxSpinnerModule,
+    CommonuiModule
   ],
 })
 export class RfqPageModule {}
