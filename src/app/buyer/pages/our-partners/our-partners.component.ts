71027ed488cab60c94fd9292b74093d9cf574b32
Pranto <prakat1620@gmail.com>
2021-03-24 20:56:27 +0600

    our partner page dynamic changes and fixes

--- src/app/buyer/pages/our-partners/our-partners.component.ts
+++ src/app/buyer/pages/our-partners/our-partners.component.ts
@@ -1,72 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectionStrategy } from '@angular/core';
 import { OurPartnersService } from '../../../shared/services/our-partners.service';
 import { config } from '../../../../config';
 import { DomSanitizer } from '@angular/platform-browser';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-our-partners',
   templateUrl: './our-partners.component.html',
   styleUrls: ['./our-partners.component.css'],
+  providers: [NgbCarouselConfig],
 })
 export class OurPartnersComponent implements OnInit {
-  currentElement: any;
-  contents: Content[];
+  //variable initialization
+  currentTab :any;
+  nextBtnDisabled:boolean=false;
+  prevBtnDisabled:boolean=true;
+slides:any;
   partnersData: any;
- headerIssue;
-  base_url = config.base_url.slice(0, config.base_url.length - 1); // / is with base_url so remove that
+  headerIssue;
+  successCaoursel:any;
+  showNavigationArrows = false;
+  showNavigationIndicators = true;
+ base_url = config.base_url.slice(0, config.base_url.length - 1); // / is with base_url so remove that
   constructor(
     private partners: OurPartnersService,
-    private sanitizer: DomSanitizer
-  ) {
-    this.contents = [
-      {
-        name: 'oem comunicao',
-        link: '../../../../assets/oem.png',
-        text:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
-      },
-      {
-        name: 'sansui',
-        link: '../../../../assets/sansui.png',
-        text:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
-      },
-      {
-        name: '96points',
-        link: '../../../../assets/96Points.png',
-        text:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
-      },
-    ];
-  }
+    private sanitizer: DomSanitizer,
+    
+/*    private changeDetection: ChangeDetectionStrategy.OnPush, */
+  ) { }
 
   ngOnInit(): void {
-    this.currentElement = this.contents[0];
+//get data from partners api
     this.partners.getOurPartnersData().subscribe((data) => {
       this.partnersData = data.data[0];
       console.log('partners', this.partnersData);
+      this.currentTab=this.partnersData.our_partner_success_story[0];
+     this.slides=this.partnersData.our_partner_success_story;
+     this.successCaoursel=this.partnersData.our_partner_slider;
       this.headerIssue = this.sanitizer.bypassSecurityTrustHtml(
         this.partnersData.sec1_description
       );
     });
+  
+    
   }
-  prevCont() {
-    this.currentElement = this.contents[0];
-  }
-  nextCont() {
-    this.currentElement = this.contents[1];
-  }
-  /*   nextCustomer() {
-      vm.index = vm.index + 1;
-      if (vm.index === vm.customers.length) {
-        vm.index = 0;
-      }
-      
-      vm.customer = vm.customers[vm.index];
-    }
-   */
+
+//previous
+prev(){
+  if (this.currentTab !== this.partnersData.our_partner_success_story[0]) {
+    const currTab = this.currentTab;
+    const i = this.slides.findIndex(function (el) {
+        return el === currTab;
+    });
+    this.currentTab = this.slides[i - 1];
+    this.nextBtnDisabled = false;
 }
-interface Content {
-  name: string;
-  link: string;
-  text: string;
+if(this.currentTab === this.partnersData.our_partner_success_story[0])  {
+  this.prevBtnDisabled = true;
 }
+}
+
+//next
+
+next() {
+  if (this.currentTab !== this.slides[this.slides.length - 1]) {
+      const currTab = this.currentTab;
+      const i = this.slides.findIndex(function (el) {
+          return el === currTab;
+      });
+      this.currentTab = this.slides[i + 1];
+      this.prevBtnDisabled = false;
+  }
+  if (this.currentTab == this.slides[this.slides.length - 1]) {
+    this.nextBtnDisabled = true;
+  }
+}
+
+
+
+}
+
