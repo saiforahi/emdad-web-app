afbc8549c5fec3640cbbf72dab77dfb87ddb44c1
pial_noman <awronno.adhar@gmail.com>
2021-03-30 09:16:24 +0600

    EW-201

--- src/app/seller/pages/seller-payment-history-page/seller-payment-history-page.component.ts
+++ src/app/seller/pages/seller-payment-history-page/seller-payment-history-page.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SubscriptionService } from 'src/app/shared/services/subscription.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+// ];
 
 @Component({
   selector: 'app-seller-payment-history-page',
@@ -21,13 +22,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./seller-payment-history-page.component.css']
 })
 export class SellerPaymentHistoryPageComponent implements OnInit {
+  subscriptionHistory: any;
 
-  constructor() { }
+  constructor(private subscription: SubscriptionService, ) { }
 
   ngOnInit(): void {
+    this.subscription.subscriptionHistory().subscribe(items => {
+      console.log(items);
+      this.subscriptionHistory = items.data[0];
+    })
   }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Payment Date', 'Subscription Plan Name', 'Expired Date'];
+  // dataSource = ELEMENT_DATA;
 
 }
--- src/app/seller/pages/subscription-plan-page/subscription-plan-page.component.ts
+++ src/app/seller/pages/subscription-plan-page/subscription-plan-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CouponService } from 'src/app/shared/services/coupon.service';
+import { SubscriptionService } from 'src/app/shared/services/subscription.service';
 
 @Component({
   selector: 'app-subscription-plan-page',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription-plan-page.component.css']
 })
 export class SubscriptionPlanPageComponent implements OnInit {
+  planList: any;
+  selectedPlanId;
+  couponButtonClicked: boolean;
+  couponInput: any = '';
+  msg: any = '';
+  discount_coupon: string;
+  couponDiscount: number = null;
+  couponType: any;
+  error: any;
 
-  constructor() { }
+  constructor(private subscription: SubscriptionService, 
+    private coupon: CouponService,) { }
 
   ngOnInit(): void {
+    this.subscription.plans().subscribe(items => {
+      console.log(items)
+      this.planList = items.data[0];
+    })
+  }
+
+  selectedPlan(index){
+    this.selectedPlanId = index;
+  }
+
+  applyCoupon() {
+    this.couponButtonClicked = true;
+    let coupon_code = this.couponInput;
+    const coupon_section = 1; // 1 for subscription, 2 for order
+    this.coupon.validateCoupon(coupon_section, coupon_code).subscribe(
+      (success) => {
+        console.log(success);
+        if (success.data == undefined) {
+          this.msg = success.message;
+          this.couponButtonClicked = false;
+          this.discount_coupon = '';
+          this.couponDiscount = null;
+        } else {
+          this.msg = `${success.data[0].coupon_title} applied!`;
+          this.couponDiscount = parseInt(success.data[0].discount_amount);
+          this.discount_coupon = success.data[0].id;
+          this.couponType = success.data[0].coupon_type;
+          // this.calcTotalPrice();
+          this.couponButtonClicked = false;
+        }
+      },
+      (error) => {
+        this.error = error;
+        console.log(error);
+        this.couponButtonClicked = false;
+      }
+    );
+  }
+
+  proceedToPay(){
+    console.log(this.couponDiscount);
+    console.log(this.planList[this.selectedPlanId])
   }
 
 }
