6370299a7d690d6a8d081e7fece06990baa6930e
Shaif Azad <shaif.rahi@northsouth.edu>
2021-04-13 03:48:19 +0600

    Buyer Profile Update Issue and translation of Raise a support ticket page wiith removing grid/list icon

--- src/app/buyer/pages/support-ticket-page/support-ticket-page.module.ts
+++ src/app/buyer/pages/support-ticket-page/support-ticket-page.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/material.module';
 import { SupportTicketPageComponent } from './support-ticket-page.component';
 import { ViewTicketDetailsComponent } from './view-ticket-details/view-ticket-details.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
   { path: '', component: SupportTicketPageComponent, pathMatch: 'full' },
@@ -12,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ViewTicketDetailsComponent],
-  imports: [CommonModule, FormsModule,MaterialModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, FormsModule,TranslateModule,MaterialModule, RouterModule.forChild(routes)],
   entryComponents: [ViewTicketDetailsComponent],
 })
 export class SupportTicketPageModule {}
