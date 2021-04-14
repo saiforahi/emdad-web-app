c6902e93ad3b6babf36dcb28f1965f4ea0602660
Shaif Azad <shaif.rahi@northsouth.edu>
2021-04-14 14:01:19 +0600

    pickup address showing in current orders and order history modal view

--- src/app/seller/pages/seller-order-history-page/order-history-modal/order-history-modal.component.ts
+++ src/app/seller/pages/seller-order-history-page/order-history-modal/order-history-modal.component.ts
@@ -4,6 +4,7 @@ import { FileService } from 'src/app/shared/services/file.service';
 import { config } from 'src/config';
 import {OrderService} from '../../../../shared/services/order.service'
 import * as fileSaver from 'file-saver';
+import { AddressService } from 'src/app/shared/services/address.service';
 @Component({
   selector: 'app-order-history-modal',
   templateUrl: './order-history-modal.component.html',
@@ -12,12 +13,26 @@ import * as fileSaver from 'file-saver';
 export class OrderHistoryModalComponent implements OnInit {
   details:any
   img_base_url:string=config.img_base_url
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private orderService:OrderService,private fileService:FileService) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private addressService:AddressService, private orderService:OrderService,private fileService:FileService) { }
 
   ngOnInit(): void {
     console.log(this.data.order)
     this.orderService.get_seller_order_details(this.data.order.order.id).subscribe(
-      (success)=> {this.details=success.data;console.log('details',success.data)}
+      (success)=> {
+        this.details=success.data;;
+        this.details.forEach((element:any) => {
+          this.addressService.get_pickup_address_details(element.pickup_address).subscribe(
+            (success)=>{
+              console.log(success.data[0])
+              if(success.data.length>0){
+                element.pickup_address=success.data[0]
+              } 
+            }
+          )
+        });
+        console.log('details',this.details)
+        //this.get_pickup_address(this.details[0].pickup_address)
+      }
     )
   }
 
@@ -47,6 +62,17 @@ export class OrderHistoryModalComponent implements OnInit {
         (error) => console.log('Error downloading the file'),
         () => console.info('File downloaded successfully');
       }
+    }
+
+    get_pickup_address(id:any){
+      this.addressService.get_pickup_address_details(id).subscribe(
+        (success)=>{
+          console.log(success.data[0])
+          if(success.data.length>0){
+            return success.data[0]
+          } 
+        }
+      )
     }
     
 }
