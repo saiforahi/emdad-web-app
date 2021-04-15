3c99edd68287c1fcac9b21688d4ef559ea5945df
pial_noman <awronno.adhar@gmail.com>
2021-04-15 20:52:12 +0600

    wishlist-bug-fix

--- src/app/buyer/pages/product-details-page/product-details-page.component.ts
+++ src/app/buyer/pages/product-details-page/product-details-page.component.ts
@@ -8,12 +8,14 @@ import { ViewportScroller } from '@angular/common';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { config } from 'src/config';
 import { CartServiceService } from 'src/app/shared/services/cart-service.service';
-import {Product} from '../../../shared/models/Product.model'
+import { Product } from '../../../shared/models/Product.model';
 import { Cart } from 'src/app/shared/models/Cart.model';
+import { UserAuthService } from 'src/app/shared/services/user-auth.service';
+import swal from 'sweetalert';
 @Component({
   selector: 'app-product-details-page',
   templateUrl: './product-details-page.component.html',
-  styleUrls: ['./product-details-page.component.css']
+  styleUrls: ['./product-details-page.component.css'],
 })
 export class ProductDetailsPageComponent implements OnInit {
   productId;
@@ -26,7 +28,7 @@ export class ProductDetailsPageComponent implements OnInit {
   totalComments = 0;
   commentlist = [];
   nextCommentsLink = null;
-  addToWishlistStatus: string = '0';
+  addToWishlistStatus: string = '1';
   screenWidth: number;
   sliderClassName: string;
   itemToshow;
@@ -34,7 +36,9 @@ export class ProductDetailsPageComponent implements OnInit {
   sliceMaxValue: number;
   activeRoute: string[];
   commnetsEnd: boolean;
-  route_subscription:any;
+  route_subscription: any;
+  uid: any;
+
   constructor(
     private getProduct: GetProductService,
     private route: ActivatedRoute,
@@ -45,44 +49,46 @@ export class ProductDetailsPageComponent implements OnInit {
     private cartService: CartServiceService,
     private spinner: NgxSpinnerService,
     private router: Router,
+    private user: UserAuthService
   ) {
     this.getScreenSize();
-    this.route_subscription=router.events.subscribe((val: any) => {
+    this.route_subscription = router.events.subscribe((val: any) => {
       if (val.url) {
         this.activeRoute = val.url.split('/');
-        if(this.activeRoute[2] == 'details' && val instanceof NavigationEnd){
+        if (this.activeRoute[2] == 'details' && val instanceof NavigationEnd) {
           this.ngOnInit();
         }
       }
     });
   }
   ngOnDestroy() {
-    // avoid memory leaks here by cleaning up after ourselves. If we  
-    // don't then we will continue to run our initialiseInvites()   
+    // avoid memory leaks here by cleaning up after ourselves. If we
+    // don't then we will continue to run our initialiseInvites()
     // method on every navigationEnd event.
-    if (this.route_subscription) {  
-       this.route_subscription.unsubscribe();
+    if (this.route_subscription) {
+      this.route_subscription.unsubscribe();
     }
   }
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.activeRoute = this.router.url.split('/');
     this.productId = this.route.snapshot.params['id'];
-    console.log('prod id',this.productId)
+    this.user.uId.subscribe((item) => {
+      this.uid = item;
+    });
+    console.log('prod id', this.productId);
     this.getProduct.productDetails(this.productId).subscribe((item) => {
       this.prodcutDetails = item.data[0];
       this.sliderImgArray = [
-        config.img_base_url +
-          item.data[0].image1,
-        config.img_base_url +
-          item.data[0].image2,
+        config.img_base_url + item.data[0].image1,
+        config.img_base_url + item.data[0].image2,
       ];
       console.log(this.prodcutDetails);
       this.getProduct
         .getProductByCategory(this.prodcutDetails.category.id)
         .subscribe((item) => {
           this.relatedProducts = item.data.results;
-           console.log('similar prod list:' ,this.relatedProducts)
+          console.log('similar prod list:', this.relatedProducts);
         });
       this.comments
         .getComments(this.prodcutDetails.id)
@@ -94,14 +100,17 @@ export class ProductDetailsPageComponent implements OnInit {
             this.commnetsEnd = true;
           }
         });
-      this.wishlist
-        .wishlistStatusCheck(this.productId)
-        .subscribe((data: any) => {
-          this.addToWishlistStatus = data.status;
-        });
+      if (localStorage.getItem('uid') != null) {
+        this.wishlist
+          .wishlistStatusCheck(this.productId)
+          .subscribe((data: any) => {
+            console.log('wishlist: ', data);
+            this.addToWishlistStatus = data.status;
+          });
+      }
     });
   }
-  
+
   scroll_to_reviews(element_id: string) {
     this.viewportScroller.scrollToAnchor(element_id);
   }
@@ -125,17 +134,17 @@ export class ProductDetailsPageComponent implements OnInit {
       this.prevBtnDisabled = true;
     }
   }
-  add_to_cart(){
-    console.log('product details',this.prodcutDetails)
-    let existingCart:Cart = JSON.parse(localStorage.getItem('cart')); //fetching existing cart from local storage
-    let existingCartLength:number=0; //initializing number of products in 
-    var foundSameProduct:boolean = false;
-    if(existingCart===null){
-      existingCart=new Cart() //initializing cart object if it's the first attempt to add to cart
+  add_to_cart() {
+    console.log('product details', this.prodcutDetails);
+    let existingCart: Cart = JSON.parse(localStorage.getItem('cart')); //fetching existing cart from local storage
+    let existingCartLength: number = 0; //initializing number of products in
+    var foundSameProduct: boolean = false;
+    if (existingCart === null) {
+      existingCart = new Cart(); //initializing cart object if it's the first attempt to add to cart
     }
-    console.log('cart',existingCart)
-    if(existingCart?.products?.length>0){
-      existingCartLength=existingCart.products.length
+    console.log('cart', existingCart);
+    if (existingCart?.products?.length > 0) {
+      existingCartLength = existingCart.products.length;
       existingCart.products.forEach((element) => {
         if (element.id == this.prodcutDetails.id) {
           foundSameProduct = true;
@@ -146,7 +155,7 @@ export class ProductDetailsPageComponent implements OnInit {
       this.prodcutDetails.cart_qty = 1;
       existingCart.products.push(this.prodcutDetails);
       this.cartService.existingCartLength.next(existingCartLength + 1);
-      console.log('cart',existingCart)
+      console.log('cart', existingCart);
       localStorage.setItem('cart', JSON.stringify(existingCart));
       this.openSnackBar('Product added to cart!', 'OK');
     } else {
@@ -198,10 +207,14 @@ export class ProductDetailsPageComponent implements OnInit {
   }
 
   addToWishlist(prod_id) {
-    this.wishlist.addTowishlist(prod_id).subscribe((item) => {
-      this.openSnackBar('Added to wishlist successfuly!', 'OK');
-      this.addToWishlistStatus = '0';
-    });
+    if (localStorage.getItem('uid') != null) {
+      this.wishlist.addTowishlist(prod_id).subscribe((item) => {
+        this.openSnackBar('Added to wishlist successfuly!', 'OK');
+        this.addToWishlistStatus = '0';
+      });
+    } else {
+      swal('Denied!', 'Please login to add to wishlist.', 'error');
+    }
   }
 
   removeFromWishlist(prod_id) {
@@ -229,25 +242,25 @@ export class ProductDetailsPageComponent implements OnInit {
   getScreenSize(event?) {
     this.screenWidth = window.innerWidth;
     // console.log(this.screenWidth);
-    if(this.screenWidth >= 300 && this.screenWidth <= 575){
+    if (this.screenWidth >= 300 && this.screenWidth <= 575) {
       this.sliderClassName = 'col-6';
       this.itemToshow = 2;
       this.sliceMinValue = 0;
       this.sliceMaxValue = 2;
       // console.log(this.sliceMinValue, this.sliceMaxValue);
-    }else if(this.screenWidth >= 576 && this.screenWidth <= 767){
+    } else if (this.screenWidth >= 576 && this.screenWidth <= 767) {
       this.sliderClassName = 'col-sm-4';
       this.itemToshow = 3;
       this.sliceMinValue = 0;
       this.sliceMaxValue = 3;
       // console.log(this.sliceMinValue, this.sliceMaxValue);
-    }else if(this.screenWidth >= 768 && this.screenWidth <= 991){
+    } else if (this.screenWidth >= 768 && this.screenWidth <= 991) {
       this.sliderClassName = 'col-md-3';
       this.itemToshow = 4;
       this.sliceMinValue = 0;
       this.sliceMaxValue = 4;
       // console.log(this.sliceMinValue, this.sliceMaxValue);
-    }else if(this.screenWidth >= 992){
+    } else if (this.screenWidth >= 992) {
       this.sliderClassName = 'col-lg-2';
       this.itemToshow = 6;
       this.sliceMinValue = 0;
@@ -256,40 +269,42 @@ export class ProductDetailsPageComponent implements OnInit {
     }
   }
 
-  nextSlide(){
+  nextSlide() {
     // console.log(this.sliceMinValue, this.sliceMaxValue, this.relatedProducts.length);
-    if(this.sliceMaxValue < this.relatedProducts.length){
+    if (this.sliceMaxValue < this.relatedProducts.length) {
       this.sliceMinValue = this.sliceMinValue + this.itemToshow;
       this.sliceMaxValue = this.sliceMaxValue + this.itemToshow;
       // console.log(this.sliceMinValue, this.sliceMaxValue);
     }
   }
 
-  prevSlide(){
+  prevSlide() {
     // console.log(this.sliceMinValue, this.sliceMaxValue, this.relatedProducts.length);
-    if(this.sliceMinValue > 0){
+    if (this.sliceMinValue > 0) {
       this.sliceMinValue = this.sliceMinValue - this.itemToshow;
       this.sliceMaxValue = this.sliceMaxValue - this.itemToshow;
       // console.log(this.sliceMinValue, this.sliceMaxValue);
     }
   }
 
-  get_unit_price(product_commission:any,price:any){ //generating unit price with commission
-    let total=0
-    if(parseFloat(product_commission)>0){
-      let unit_price=parseFloat(price)* (parseFloat(product_commission) / 100)
-      total = unit_price + parseFloat(price)
+  get_unit_price(product_commission: any, price: any) {
+    //generating unit price with commission
+    let total = 0;
+    if (parseFloat(product_commission) > 0) {
+      let unit_price =
+        parseFloat(price) * (parseFloat(product_commission) / 100);
+      total = unit_price + parseFloat(price);
       //console.log('if total',total)
       //console.log('unit price',(parseFloat(unit_price) * (product_commission / 100))+parseFloat(unit_price))
       //return (parseFloat(unit_price) * (product_commission / 100))+parseFloat(unit_price)
-    }
-    else{
-      
-      let unit_price=parseFloat(price)* (parseFloat(localStorage.getItem('commission')) / 100)
-      total = unit_price + parseFloat(price)
+    } else {
+      let unit_price =
+        parseFloat(price) *
+        (parseFloat(localStorage.getItem('commission')) / 100);
+      total = unit_price + parseFloat(price);
       //console.log('else total',total)
     }
     //console.log(total.toFixed(2))
-    return total.toFixed(2)
+    return total.toFixed(2);
   }
 }
