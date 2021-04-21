88173e3ed85ca1e861cd400aa4748017f244a5f2
Pranto <prakat1620@gmail.com>
2021-04-22 00:28:14 +0600

    forgot pass page for buyer responsive,design modification as per figma,translation

--- src/app/buyer/pages/forget-password-page/forget-password-page.module.ts
+++ src/app/buyer/pages/forget-password-page/forget-password-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ForgetPasswordPageComponent } from './forget-password-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonuiModule } from 'src/app/ui/commonui/commonui.module';
 import { ForgetPsswordFormComponent } from '../../components/forget-pssword-form/forget-pssword-form.component';
 import { NgxSpinnerModule } from "ngx-spinner";
 const routes: Routes = [
@@ -14,7 +15,7 @@ const routes: Routes = [
     ForgetPasswordPageComponent,
     ForgetPsswordFormComponent
   ],
-  imports: [CommonModule,NgxSpinnerModule, FormsModule, RouterModule.forChild(routes)],
+  imports: [CommonModule,NgxSpinnerModule, FormsModule,CommonuiModule, RouterModule.forChild(routes)],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ForgetPasswordPageModule {}
