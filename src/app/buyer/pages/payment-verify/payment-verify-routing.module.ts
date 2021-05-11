9cb18c94860cac497682920969971f086e9e5b13
Pranto <prakat1620@gmail.com>
2021-05-11 21:15:23 +0600

    verify payment page translation and sller registration expiry date safari issue error fix attempt 1

--- src/app/buyer/pages/payment-verify/payment-verify-routing.module.ts
+++ src/app/buyer/pages/payment-verify/payment-verify-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { PaymentVerifyComponent } from './payment-verify.component';
 
 const routes: Routes = [{ path: '', component: PaymentVerifyComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes), TranslateModule],
+  exports: [RouterModule, TranslateModule]
 })
 export class PaymentVerifyRoutingModule { }
